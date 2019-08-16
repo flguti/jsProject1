81370d2a368d6b846f555343efb42e849722f3dd
Flavio Lopes <flaviolopes@Flavios-MacBook-Air.local>
2019-08-15 21:06:27 -0400

    initial

---
+++ script.js
@@ -0,0 +1,41 @@
+//Create array for words
+const words = ["await", "abstract", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "debugger", "default", "delete", "do", "double", "else", "enum", "export", "extends", "false", "final", "finally", "float", "for", "function", "gogo", "if", "implements", "import", "in", "instanceof", "int", "interface", "let", "long", "native", "new", "null", "package", "protected", "private", "prototype", "public", "return", "short", "super", "static", "switch", "synchronized", "this", "throw", "throws", "true", "transient", "try", "typeof", "undefined", "var", "void", "volatile", "while", "with", "yield"];
+    
+//Choose word randomly
+const word = words[Math.floor(Math.random() * words.length)];
+
+//Make array for answer
+let answer = [];
+    for (let i = 0; i < word.length; i++) {
+        answer[i] = "_";
+    }
+
+let remainingLetters = word.length;
+    
+    //The playing loop
+    while (remainingLetters > 0) {
+        //Show players progress
+        alert("This is the number of remaining letters:\n" + answer.join(" "));
+        
+        //Prompt player to guess
+        let guess = prompt("Guess a letter or click 'Cancel' to stop the game.");
+        if (prompt === null) {
+            //Leave the game
+            break;
+        } else if (guess.length !== 1) {
+            alert("Please enter one single letter.");
+        } else {
+            //Update match with guess
+            for (let j = 0; j < word.length; j++) {
+                if (word[j] === guess) {
+                    answer[j] = guess;
+                    remainingLetters--;
+                }
+            }
+        }
+        //End of playing loop
+    }
+    
+    //Show answer and congratulate the player
+    alert(answer.join(" "));
+    alert("Good work! The right answer is " + word);
